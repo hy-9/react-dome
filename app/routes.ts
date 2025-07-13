f3203691fbabe3b9e3c3d1a86b0e0ec56df5691f
HaiYao <2019086272@qq.com>
2025-07-13 12:20:29 +0800

    路由初始化

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,22 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import {
+	type RouteConfig,
+	index,
+	layout,
+	prefix,
+	route,
+} from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+	// 默认页面
+	index("routes/logIn.tsx"),
+	// 前缀
+	...prefix("app", [
+		// 布局， 有这个才可以用 Outlet 组件渲染子路由组件
+		layout("routes/app.tsx", [
+			// 子路由
+			route("main", "routes/app/main.tsx"),
+			route("detail", "routes/app/detail.tsx"),
+		]),
+	]),
+	// layout("routes/app.tsx", [route("main", "routes/app/main.tsx")]),
+] as RouteConfig;
---
+++ app/routes/app.tsx
@@ -0,0 +1,33 @@
+import { Outlet } from "react-router";
+
+export default function App() {
+	return (
+		<div className="h-full w-full">
+			<div className="flex h-12 w-full bg-gray-100">
+				<div className="h-full flex-1 bg-amber-200"></div>
+				<div className="mx-2 flex h-full w-20 items-center justify-center bg-sky-300">
+					<a href="/">登录</a>
+				</div>
+			</div>
+			<div className="flex h-[calc(100vh-calc(var(--spacing)*12))] w-full bg-green-100">
+				<div className="h-full w-24 bg-fuchsia-300">
+					<nav className="flex h-full w-24 flex-col">
+						<a
+							href="/app/main"
+							className="flex h-14 items-center px-4 text-gray-500 hover:bg-gray-200">
+							主页
+						</a>
+						<a
+							href="/app/detail"
+							className="flex h-14 items-center px-4 text-gray-500 hover:bg-gray-200">
+							详情
+						</a>
+					</nav>
+				</div>
+				<div className="h-full flex-1">
+					<Outlet />
+				</div>
+			</div>
+		</div>
+	);
+}
